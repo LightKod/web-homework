566b95bc3340a5ddc024b4f8080bbe9b37ddf8b3
Quang <nguyennhatquangkod2003@gmail.com>
2024-10-23 07:48:11 +0700

    clean up

--- ExpressServer/src/database.js
+++ ExpressServer/src/database.js
@@ -1,17 +1,21 @@
 import knex from 'knex'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = knex({
-    client: 'mysql2',
-    connection: {
-        host: 'localhost',
-        user: 'root',
-        password: '123456',
-        database: 'sakila',
-        port: 3306,
-    },
-    pool: {
-        min: 0, max: 10,
-    },
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+  pool: {
+    min: 0,
+    max: 10,
+  },
 });
 
 db.raw('SELECT 1 + 1 AS result')
